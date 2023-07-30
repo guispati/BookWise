272438871259c685ec86981cb113fc45a70e6293
Guilherme Spati <guilherme.spati723@gmail.com>
2023-07-30 16:27:43 -0300

    initial config

---
+++ src/pages/layouts/DefaultLayout/index.tsx
@@ -0,0 +1,16 @@
+import Head from "next/head";
+import { DefaultLayoutContent, DefaultLayoutContainer } from "./styles";
+import type { DefaultLayoutProps } from "./types";
+
+export const DefaultLayout = ({ title, children }: DefaultLayoutProps) => {
+  return (
+    <DefaultLayoutContainer>
+        <Head>
+            <title>{`${title} | BookWise`}</title>
+            <link rel="shortcut icon" href="/favicon.svg" type="image/svg" />
+        </Head>
+        
+        <DefaultLayoutContent>{children}</DefaultLayoutContent>
+    </DefaultLayoutContainer>
+  );
+};
